c700060354dd19929457087afafee7d7a2f8f4d2
hanisx019 <anisx019@gmail.com>
2026-05-27 02:40:33 +0530

    Update HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,11 @@
+import { MailIcon } from "@animateicons/react/lucide";
+import { InstagramIcon } from "@animateicons/react/lucide";
 import StarPattern from '../components/StarPattern';
+import { GithubIcon } from "@animateicons/react/lucide";
+import { LinkedinIcon } from "@animateicons/react/lucide";
+import { TwitterIcon } from "@animateicons/react/lucide";
+import { FacebookIcon } from "@animateicons/react/lucide";
+import { ShareIcon } from "@animateicons/react/lucide";
 // import TextScramble from '../animation/TextScramble';
 // import TextLoop from '../components/motion-primitives/text-loop';
 
@@ -13,21 +20,29 @@ const HomePage = () => {
         <p id="font-rem" className="text-md text-gray-400 text-left tracking-wider">
         Helping brands and businesses transform ideas into visually stunning digital products, design and develop immersive digital experiences that blend creativity, performance, and modern interaction.
         </p>
+
+        <div className="absolute top-[380px] left-1/2 transform -translate-x-1/2 z-10 text-purple-500 text-nowrap">
+          <TwitterIcon className="mx-2" size={30} duration={1} />
+          <MailIcon className="mx-2" size={30} duration={1} />
+          <InstagramIcon className="mx-2" size={30} duration={1} />
+          <GithubIcon className="mx-2" size={30} duration={1} />
+          <LinkedinIcon className="mx-2" size={30} duration={1}/>
+          <FacebookIcon sclassName="mx-2" size={30} duration={1}/>
+          <ShareIcon className="mx-2" size={30} duration={1}/>
+         </div>
+
       </div>
       
-      <div>
-        
-      </div>
 
       <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-10 text-nowrap top-1/3">
         <div className="pointer-events-auto text-center transform -translate-y-1/2">
           <div className="inline-flex flex-wrap items-center justify-center gap-4 animate-fade-up">
-            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-7xl text-center font-thin text-white">STRATEGIC</p>
-            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-7xl text-center font-thin text-white">DESIGN<span className="text-cyan-400">.</span></p>
+            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-8xl text-center font-thin text-white">STRATEGIC</p>
+            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-8xl text-center font-thin text-white">DESIGN<span className="text-cyan-400">.</span></p>
           </div><br></br>
           <div className="inline-flex flex-wrap items-center justify-center gap-4 animate-fade-up animate-fade-up-delay-200">
-            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-7xl text-center font-thin text-white">SEAMLESS</p>
-            <p id="font-rem" className="inline-block tracking-wider text-2xl md:text-7xl text-center font-normal bg-gradient-to-r from-purple-500 via-blue-500 to-green-400 text-transparent bg-clip-text">DEVELOPMENT</p>
+            <p id="font-rem" className="tracking-wider text-pretty text-2xl md:text-8xl text-center font-thin text-white">SEAMLESS</p>
+            <p id="font-rem" className="inline-block tracking-wider text-2xl md:text-8xl text-center font-normal bg-gradient-to-r from-purple-500 via-blue-500 to-green-400 text-transparent bg-clip-text">DEVELOPMENT</p>
           </div>
         </div>
       </div>
