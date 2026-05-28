091db422fab9c8cd69393eb366cac522e679ee7d
hanisx019 <anisx019@gmail.com>
2026-05-28 20:49:39 +0530

    Update Homepage

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,8 @@
 import Animation from './animation.mp4';
-import Video from './video.mp4';
+import temp1 from './home-work1.jpg';
+import temp2 from './home-work2.jpg';
+import temp3 from './home-work3.jpg';
+import temp4 from './home-work4.jpg';
+import temp5 from './home-work5.jpg';
 
-export { Animation , Video};
+export { Animation , temp1 , temp2, temp3, temp4, temp5};
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,8 @@ import { TwitterIcon } from "@animateicons/react/lucide";
 import ScrollFloat from '../animation/ScrollFloat';
 import ScrollReveal from '../animation/ScrollReveal';
 import CurvedLoop from '../animation/CurvedLoop';
-import Folder from '../animation/Folder'
+import Folder from '../animation/Folder';
+import { temp1, temp2, temp3, temp4, temp5 } from '../assets/assets';
 // import TextScramble from '../animation/TextScramble';
 // import TextLoop from '../components/motion-primitives/text-loop';
 
@@ -47,7 +48,7 @@ const HomePage = () => {
       </div>
 
       {/*The Vision*/}
-      <div id="font-rem" className="pb-56 text-black pt-[150px] bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 flex flex-col justify-center relative">
+      <div id="font-rem" className="pb-56 px-16 text-black pt-[150px] bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 flex flex-col justify-center relative">
           <ScrollFloat className="top-[28%] pt-60" animationDuration={1} ease='back.inOut(2)' scrollStart='center bottom+=50%' scrollEnd='bottom bottom-=40%' stagger={0.03}>
              The Vision  
           </ScrollFloat>
@@ -60,22 +61,76 @@ const HomePage = () => {
 
       </div>
 
+      {/*SERVICES*/}
+      <div className="px-16 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
+        <h2 id="font-rem" className=" text-9xl font-bold  mb-8 text-black pt-[150px] text-left ">I help companies to succeed on projects like:</h2>
+
+        <div className="mt-44">
+          <center className=" text-6xl font-bold text-black ">Websites &<br/> Landing pages</center>
+          <center id="font-rem" className="mt-8  text-xl text-black font-semibold">Creating high-end and beautiful websites built to perform and convert.</center>
+
+          <div className="w-full mt-8 flex justify-center items-center gap-4">
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp1} alt="Project 1" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp2} alt="Project 2" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp3} alt="Project 3" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp4} alt="Project 4" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp5} alt="Project 5" /></div>
+          </div>
+        </div>
+
+        <div className="mt-48">
+          <center className=" text-6xl font-bold text-black ">Visual Branding</center>
+          <center id="font-rem" className="mt-8  text-xl text-black font-semibold">Helping brands find a distinctive visual language that truly stands out.</center>
+
+          <div className="w-full mt-8 flex justify-center items-center gap-4">
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp1} alt="Project 1" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp2} alt="Project 2" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp3} alt="Project 3" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp4} alt="Project 4" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp5} alt="Project 5" /></div>
+          </div>
+        </div>
+
+        <div className="mt-48">
+          <center className=" text-6xl font-bold text-black ">Product Design <br/>Enhancement</center>
+          <center id="font-rem" className="mt-8  text-xl text-black font-semibold">Bringing fresh ideas to turn complex products into intuitive experiences with an elevated visual layer.</center>
+
+          <div className="w-full mt-8 flex justify-center items-center gap-4">
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp1} alt="Project 1" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp2} alt="Project 2" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp3} alt="Project 3" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp4} alt="Project 4" /></div>
+            <div className="w-1/5 rounded-2xl"><img className="rounded-2xl" src={temp5} alt="Project 5" /></div>
+          </div>
+        </div>
+
+        <div className="mt-48">
+          <center className=" text-6xl font-bold text-black ">Webflow & <br/>Framer</center>
+          <center id="font-rem" className="mt-8  text-xl text-black font-semibold">Building elegant and responsive projects featuring creative micro-interactions and seamless CMS hand-off.</center>
+          <div className="select-none">
+          <div className="min-h-screen pb-56 text-black  flex flex-col justify-center relative z-30 overflow-visible">
+            <div className="flex items-center justify-center relative w-full" style={{ minHeight: '80vh' }}>
+              <h2 id="font-rem" className="text-[7rem] md:text-[12rem] lg:text-[14rem] font-extrabold tracking-[0.4em] text-center flex items-center gap-6 leading-none max-w-full">
+              <span className="text-[350px] pl-4">W</span>
+              <span className="inline-flex items-center justify-center w-32 h-32 md:w-48 md:h-48 lg:w-56 lg:h-56 mr-20">
+                <Folder size={3.5} gradient="linear-gradient(90deg, #a855f7, #3b82f6, #22c55e)" className="custom-folder inline-block" />
+              </span>
+              <span className="text-[350px]">r</span>
+              <span className="text-[350px]">k</span>
+              </h2>
+            </div>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      </div>
 
 
       {/*WORK*/}
-      <div className="select-none">
-          <div className="min-h-screen pb-56 text-white pt-24 bg-black flex flex-col justify-center relative z-30 overflow-visible">
-            <div className="flex items-center justify-center relative w-full" style={{ minHeight: '80vh' }}>
-              <h2 id="font-rem" className="text-[7rem] md:text-[12rem] lg:text-[14rem] font-extrabold tracking-[0.4em] text-center flex items-center gap-6 leading-none max-w-full">
-                <span>W</span>
-                <span className="inline-flex items-center justify-center w-32 h-32 md:w-48 md:h-48 lg:w-56 lg:h-56 mr-20">
-                  <Folder size={3.5} gradient="linear-gradient(90deg, #a855f7, #3b82f6, #22c55e)" className="custom-folder inline-block" />
-                </span>
-                <span>RK</span>
-              </h2>
-            </div>
-          </div>
-      </div>
+
         
         {/*VIDEO*/}
 
